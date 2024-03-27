0242a90acbce9635c1a42fa4affcb62557b7904b
Tim Huang <ty2huang@edu.uwaterloo.ca>
2024-03-27 15:43:14 -0400

    New "SelectDataSource" and custom "CreateFromSource" method for MultiSelectParameter

--- squirrels/__init__.py
+++ squirrels/__init__.py
@@ -4,5 +4,5 @@ from .arguments.init_time_args import ConnectionsArgs, ParametersArgs
 from .arguments.run_time_args import AuthArgs, ContextArgs, ModelDepsArgs, ModelArgs
 from .parameter_options import SelectParameterOption, DateParameterOption, DateRangeParameterOption, NumberParameterOption, NumberRangeParameterOption
 from .parameters import SingleSelectParameter, MultiSelectParameter, DateParameter, DateRangeParameter, NumberParameter, NumberRangeParameter
-from .data_sources import SingleSelectDataSource, MultiSelectDataSource, DateDataSource, DateRangeDataSource, NumberDataSource, NumberRangeDataSource
+from .data_sources import SingleSelectDataSource, MultiSelectDataSource, SelectDataSource, DateDataSource, DateRangeDataSource, NumberDataSource, NumberRangeDataSource
 from .user_base import User, WrongPassword
